5c3e8b26cbfea1fcb06cf9a64b5b980bb249a544
sanaulla0 <vadapallisanaulla@gmail.com>
2023-03-21 17:43:53 +0530

    p2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [formdata, setFormdata] = useState({
+     Email : '',
+     Password : '',
+  });
+  const [error,setError] = useState(false);
+  const handleChange =(event)=>{
+
+      const {name,value} = event.target;
+
+       const update = {...formdata};
+       update[name]= value;
+       setFormdata(update);
+         
+
+  }
+  const handleSubmit = ()=>{
+    if(!formdata.Email || !formdata.Password){
+        setError(true);
+}
+else
+{
+  setError(false);
+  }
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+
+      <span>UserName</span>  <input type="email"  name="Email" value={formdata.Email} onChange={handleChange}  />  <br/>
+      <span>Password</span>  <input type="password" value={formdata.Password} name="Password" onChange={handleChange}  />  <br/>
+
+        <h4 id="errorMessage">{error ? 'Both username and password are required' : ''}</h4>
+      <button type="submit" onClick={handleSubmit}
+      >Login</button>
+     
+      <hr/>
+
     </div>
   )
-}
 
-export default App
+  
+}
+export default App;
+
